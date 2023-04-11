7042d5179135c5a92d4c522e186ffa56a878490c
Jeff Cameron <jeffreycameron85@gmail.com>
2023-04-11 22:04:49 +1000

    Adding Gym Exercise App Project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ReactDOM } from 'react'
+
+import App from './App'
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+
+root.render(
+    <App />
+)
